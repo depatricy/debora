a89e3f9164d240f8de6c2a909db8680186051e67
depatricy <99355554+depatricy@users.noreply.github.com>
2023-06-06 00:47:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Serviços</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="materialize/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+
+        <div class="card-panel cyan">
+        <h2 class="header center white-text">Olá! Bem vindo(a)!</h2>
+        <div class="row center">
+       
+        </div>
+      
+        <div class="row center">
+          <h7>Clique abaixo para ser redirecionado à uma conversa</h7><p></p>
+          <a href="https://api.whatsapp.com/send?phone=5588993304957&text=Oi,%20estou%20com%20interesse%20em..." id="download-button" class="btn-small waves-effect waves-light purple lighten-3">Coonverse comigo pelo whatsapp</a>
+        </div>
+      </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="imagens/smile.png"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+
+            <table>
+        <thead>
+          <tr>
+              <th>Serviço</th>
+              <th>Tipo</th>
+              <th>Preço</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Xerox</td>
+            <td>Preto e branco</td>
+            <td>R$0,25</td>
+          </tr>
+          <tr>
+            <td>Xerox</td>
+            <td>Colorida</td>
+            <td>R$0,50</td>
+          </tr>
+          <tr>
+            <td>Impressão</td>
+            <td>Preto e branco</td>
+            <td>R$0,50</td>
+          </tr>
+          <tr>
+            <td>Impressão</td>
+            <td>Colorida</td>
+            <td>R$1,00</td>
+          </tr>
+          <tr>
+            <td>Digitação</td>
+            <td>Por folha</td>
+            <td>R$1.00</td>
+          </tr>
+          <tr>
+            <td>Elaboração de currículo</td>
+            <td>-</td>
+            <td>R$10.00</td>
+          </tr>
+          <tr>
+            <td>Slides</td>
+            <td>Por página</td>
+            <td>R$1.00</td>
+          </tr>
+           <tr>
+            <td>Slides COMPLETOS</td>
+            <td>-</td>
+            <td>R$15.00</td>
+          </tr>
+          <tr>
+            <td>Pesquisa/Trabalho Escolar+Impressão</td>
+            <td>Por folha</td>
+            <td>R$1.00</td>
+          </tr>
+
+          <tr>
+            <td>Pesquisa/Trabalho Escolar COMPLETO+Impressão</td>
+            <td>-</td>
+            <td>R$15.00</td>
+          </tr>
+        </tbody>
+      </table>
+           
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  
+
+
+ 
+
+
+  <!--  Scripts-->
+  
+   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="materialize/js/materialize.js"></script>
+  <script src="materialize/js/init.js"></script>
+  <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+  </body>
+</html>
